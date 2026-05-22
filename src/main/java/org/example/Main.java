a79bfa8646bc5d0cd32c75972b45f478daa4c0f8
xiomarariverav <XIomara1429@gmail.com>
2026-05-22 10:48:54 -0400

    tested CSV menu loading in Main class

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,9 +1,18 @@
 package org.example;
 
+import java.util.ArrayList;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
 
+        ArrayList<Ingredient> ingredients =
+                MenuLoader.loadingIngredient("src/main/resources/cafe_menu_items.csv");
+
+        for(Ingredient ingredient : ingredients){
+
+            System.out.println(ingredient.getName());
+        }
     }
 }
