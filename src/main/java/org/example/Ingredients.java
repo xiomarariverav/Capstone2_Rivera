fdf9a80967155ee780dcc30834a7506d4461c15b
xiomarariverav <XIomara1429@gmail.com>
2026-05-22 09:54:05 -0400

    Started pojo variables

--- src/main/java/org/example/Ingredients.java
+++ src/main/java/org/example/Ingredients.java
@@ -1,4 +1,11 @@
 package org.example;
 
+import java.math.BigDecimal;
+
 public class Ingredients {
+    private String name;
+    private String type;
+    private BigDecimal smallPrice;
+    private BigDecimal mediumPrice;
+    private BigDecimal largePrice;
 }
