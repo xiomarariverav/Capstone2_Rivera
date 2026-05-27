6b52d4b5f9ffa34cb53de874dcaf9218eff5e8e7
xiomarariverav <XIomara1429@gmail.com>
2026-05-27 09:29:13 -0400

    Made signature coffee drink classes

---
+++ src/main/java/org/example/CloudMatcha.java
@@ -0,0 +1,8 @@
+package org.example;
+
+public class CloudMatcha extends CoffeeDrink {
+
+    public CloudMatcha(String size, String temperature) {
+        super("Cloud Matcha", size, temperature);
+    }
+}
---
+++ src/main/java/org/example/MochaDream.java
@@ -0,0 +1,8 @@
+package org.example;
+
+public class MochaDream extends CoffeeDrink {
+
+    public MochaDream(String size, String temperature) {
+        super("Mocha Dream", size, temperature);
+    }
+}
